653985932f0d0b1b74851322a673c9422dbf88c6
idattijo4code <idattijo@gmail.com>
2023-07-06 13:52:44 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# How to run
+
+- setup the .env file and provide the database 
+In the Terminal
+- Run `composer install`
+- Run `php artisan serve`
+
---
+++ routes/web.php
@@ -0,0 +1,42 @@
+<?php
+
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::resource('products', 'ProductController');
+
+Route::get('/cart', 'CartController@index')->name('cart.index');
+Route::post('/cart/add/{productId}', 'CartController@add')->name('cart.add');
+Route::post('/cart/update/{productId}', 'CartController@update')->name('cart.update');
+
+Route::resource('orders', 'ProductOrderController')->only(['show', 'store']);
+
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+
+});
+
+require __DIR__.'/auth.php';
